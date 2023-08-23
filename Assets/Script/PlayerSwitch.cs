99778055c6a58eac5ca9d171a5405117247251ff
laylaFara <65655509+laylaFara@users.noreply.github.com>
2023-08-23 16:21:10 +0200

    Script

--- Assets/Script/PlayerSwitch.cs
+++ Assets/Script/PlayerSwitch.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerSwitch : MonoBehaviour
 {
     public HumanMove humancontroller;
+    public ChockenMove chickencontroller;
+    public bool Player1active = true;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,30 @@ public class PlayerSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            SwitchPlayer();
+        }
     }
-}
+
+    public void SwitchPlayer()
+    {
+
+
+
+        if (Player1active)
+        {
+            humancontroller.enabled = false;
+            chickencontroller.enabled = true;
+            Player1active = false;
+        }
+
+        else
+        {
+            humancontroller.enabled = true;
+            chickencontroller.enabled = false;
+            Player1active = true;
+        }
+    }
+    }
+
