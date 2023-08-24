3ef44e134d3fd318e2278c24d0c7707ee0e5604f
laylaFara <65655509+laylaFara@users.noreply.github.com>
2023-08-24 17:18:34 +0200

    Thursday Final

--- Assets/Script/ChockenMove.cs
+++ Assets/Script/ChockenMove.cs
@@ -15,18 +15,34 @@ public class ChockenMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var animator = GetComponent<Animator>();
+        var horiz = Input.GetAxis("Horizontal");
         jump();
-        Vector3 movment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        Vector3 movment = new Vector3(horiz, 0f, 0f);
         transform.position += movment * Time.deltaTime * moveSpeed;
+
+        if (Mathf.Approximately(horiz, 0f))
+        {
+            animator.SetBool("ChickRun", false);
+
+        }
+        else
+        {
+            animator.SetBool("ChickRun", true);
+        }
+
 
 
     }
 
     void jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        var animator = GetComponent<Animator>();
+        var jumping = Input.GetButtonDown("Jump");
+        if (jumping && isGrounded)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 9f), ForceMode2D.Impulse);
         }
+        animator.SetBool("ChickJump", !isGrounded);
     }
 }
