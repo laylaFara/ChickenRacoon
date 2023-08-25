1fe6af5b7e36270eda4cc82034f1c49bc2518011
laylaFara <65655509+laylaFara@users.noreply.github.com>
2023-08-25 17:07:34 +0200

    Last verzen

---
+++ Assets/Script/rakkill.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class rakkill : MonoBehaviour {
+
+	//private BrojacZivota brojacZivota;
+
+	//void Awake() 
+//	{
+//		brojacZivota = GameObject.Find ("brojZivota").GetComponent<BrojacZivota> ();
+//	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+		void OnCollisionEnter2D(Collision2D other)
+   {
+        if (other.gameObject.tag == "Chicken")
+        {
+			Smrtabc(other.gameObject);
+			GetComponent<Rigidbody2D>().velocity = transform.right * 10f;
+		}
+     }
+
+	public void Smrtabc(GameObject igracabc) {
+		//brojacZivota.ukupnoZivota--;
+	    igracabc.transform.position = new Vector3 (-22.08f, -5.16f, 0);
+		//if (brojacZivota.ukupnoZivota == 0) {
+		//	SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+		//}
+	}
+
+}
