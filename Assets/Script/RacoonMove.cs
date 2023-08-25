6b88c47c441b689754d76712be9140016f8990cb
laylaFara <65655509+laylaFara@users.noreply.github.com>
2023-08-25 10:35:32 +0200

    Raccoon-first Try

--- Assets/Script/RacoonMove.cs
+++ Assets/Script/RacoonMove.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class RacoonMove : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = transform.up * 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
