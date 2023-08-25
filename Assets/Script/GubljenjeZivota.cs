3d28758e3a10f5befdc1dc1e9b03a438fb0a54af
JelenaCndy <142896286+JelenaCndy@users.noreply.github.com>
2023-08-25 13:32:04 +0200

    1st lvl

---
+++ Assets/Script/GubljenjeZivota.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class GubljenjeZivota : MonoBehaviour {
+
+	//private BrojacZivota brojacZivota;
+	public Transform igrac;
+
+	//void Awake() 
+//	{
+//		brojacZivota = GameObject.Find ("brojZivota").GetComponent<BrojacZivota> ();
+//	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+		void OnTriggerEnter2D(Collider2D other)
+   {
+        if (other.gameObject.tag == "Player")
+        {
+			Smrt();
+        }
+     }
+
+	public void Smrt() {
+		//brojacZivota.ukupnoZivota--;
+	    igrac.transform.position = new Vector3 (-22.08f, -5.16f, 0);
+		//if (brojacZivota.ukupnoZivota == 0) {
+		//	SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+		//}
+	}
+
+}
