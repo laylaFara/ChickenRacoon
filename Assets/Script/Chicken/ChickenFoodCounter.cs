02487311a74a3d270c16ab7dd8040f7b2028f60e
laylaFara <65655509+laylaFara@users.noreply.github.com>
2023-08-25 15:13:14 +0200

    Layla Push

--- Assets/Script/Chicken/ChickenFoodCounter.cs
+++ Assets/Script/Chicken/ChickenFoodCounter.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HumanFoodCounter : MonoBehaviour
+public class ChickenFoodCounter : MonoBehaviour
 {
     public int counter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Text>().text = counter.ToString();
+         GetComponent<Text>().text = counter.ToString();
     }
 }
